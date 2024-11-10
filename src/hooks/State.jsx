b3155f31cd151dfb24b35c3927da4f5c03d80429
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-10 18:04:05 +0530

    changes usestate

--- src/hooks/State.jsx
+++ src/hooks/State.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import styles from '../hooks/State.module.css';
+
 export const UseStateHook = () => {
     const [preVal, newVal] = useState(0);
     
@@ -15,9 +16,18 @@ export const UseStateHook = () => {
 
         <>
         <div className={styles.container}>
-        <div className={styles.count}>{preVal}</div>
-        <button className={styles.buttonContainer} onClick={inc}>Increment</button>
+            <div className={styles.count}>{preVal}</div>
+            <button className={styles.buttonContainer} onClick={inc}>Increment</button>
+            <ChildComponent count={preVal}/>
         </div>
+        
         </>
+    )
+}
+
+// eslint-disable-next-line react/prop-types
+const ChildComponent = ({count}) =>{
+    return (
+        <h2>This is Child Component Count{` - ${count}`}</h2>
     )
 } 
